d8ef8cb8b1c92ef6323f89615a8d068dd59f9c41
Hiba <hibat64@gmail.com>
2020-07-15 10:26:01 +0100

    modif1

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
+
 let pets = [
   { name: "Max", type: "dog", bornOn: 2018 },
   { name: "Angel", type: "cat", bornOn: 2015 },
   { name: "Jasper", type: "dog", bornOn: 2016 }
 ];
 
-const getAge=(pet)=> {
+const getAge = (pet) => {
   return new Date().getFullYear() - pet.bornOn;
 }
 
-let petsWithAge = [];
-pets.map(pet=>
- {
-   pet.age = getAge(pet);})
- pets.forEach(pet=>
-  {petsWithAge.push(pet);
-  })
+let petsWithAge = []; {
+  pets.map(pet =>
+      pet.age = getAge(pet))
+  pets.forEach(el => petsWithAge.pusg(el))
+  console.log(petsWithAge)
+
+}
 console.log(petsWithAge);
 
 let dogs = [];
-pets.filter(pet =>
-(pet.type === "dog") 
-  
-)
-console.log(dogs);
+pets.filter(pet => pet.type == "dog")
+
+console.log(dogs)
 
 
-let jasper = pets.find(pet => pet.name === `Jasper`)
-console.log(`Jasper is ${jasper.age} years old`);
+const jasper = pets.find(pet => { return pet.name == "jasper" })
+
+
+console.log("Jasper is ${jasper.age} years old");
